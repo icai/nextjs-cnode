b38668a8c11a6fedbaa6c75addcf7f0c09783c9a
Terry Cai <gidcai@gmail.com>
2018-09-24 02:18:46 +0800

    更新px2rem适配组件

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -11,11 +11,14 @@ export default class Layout extends Component<{
         const { children, className, title } = this.props;
         const clsName = className || "flex-wrp" 
 
+        const staticMarkup = `!function(x){function w() { var v, u, t, tes, s = x.document, r = s.documentElement, a = r.getBoundingClientRect().width; if (!v && !u) { var n = !!x.navigator.appVersion.match(/AppleWebKit.*Mobile.*/); v = x.devicePixelRatio; tes = x.devicePixelRatio; v = n ? v : 1, u = 1 / v } if (a >= 640) { r.style.fontSize = "40px" } else { if (a <= 320) { r.style.fontSize = "20px" } else { r.style.fontSize = a / 320 * 20 + "px" } } }x.addEventListener("resize",function(){w()});w()}(window);`
+    
         return <View className={clsName}>
             <Head>
               <meta charSet="utf-8" />
               <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                {title ? <title>{title}</title>: ''}
+              {title ? <title>{title}</title> : ""}
+              <script dangerouslySetInnerHTML={{ __html: staticMarkup }}></script>
             </Head>
             {children}
           </View>;
