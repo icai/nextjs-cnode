2ec745f08ae39c1a395a761c837d5d320e6c34ad
Terry Cai <gidcai@gmail.com>
2018-09-26 18:23:42 +0800

    添加缓存

---
+++ src/cache/index.ts
@@ -0,0 +1,24 @@
+import LRU from "lru-cache";
+// github.com/isaacs/node-lru-cache
+
+const options = {
+    max: 500,
+    length: function(n, key) {
+      return n * 2 + key.length;
+    },
+    dispose: function(key, n) {
+      n.close();
+    },
+    maxAge: 1000 * 60 * 60
+  },
+  cache = LRU(options); // sets just the max size
+
+export default cache;
+
+// export const set = (key, value, maxAge?) => {
+//     return cache.set(key, value, maxAge);
+// };
+
+// export const get = (key) => {
+//   return cache.get(key);
+// };
